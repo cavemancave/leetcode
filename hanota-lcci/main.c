ceafadffc230c9a4a05d9619b45d845295258ceb
cavemancave <948238104@qq.com>
2020-09-20 15:49:05 +0800

    add hanota-lcci

---
+++ hanota-lcci/main.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAXLEN 200
+typedef struct {
+    int data[MAXLEN];
+    int cur;
+    char name;
+}Stack;
+
+
+void Push(Stack *stack, int d)
+{
+    stack->data[stack->cur++] = d;
+}
+
+int Pop(Stack *stack)
+{
+    return stack->data[stack->cur--];
+}
+
+int Top(Stack *stack)
+{
+    return stack->data[stack->cur];
+}
+
+void hannuota(int n, Stack* A, Stack* B, Stack* C)
+{
+    if(n == 1){
+        printf("move %d form %c to %c.\n", Top(A), C->name, A->name);
+        Push(C, Pop(A));
+    }else{
+        hannuota(n-1, A, C, B);
+        hannuota(1, A, B, C);
+        hannuota(n-1, B, A, C);
+    }
+    
+}
+int main()
+{
+    int n = 1;
+    Stack A = {0};
+    Stack B = {0};
+    Stack C = {0};
+    A.name = 'A';
+    B.name = 'B';
+    C.name = 'C';
+    hannuota(n, &A, &B, &C);
+
+    return 0;
+}
