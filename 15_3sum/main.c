b9472f429bcc961a38a9fdd0bbb266eb70fcb203
cavemancave <948238104@qq.com>
2021-03-21 04:20:14 +0000

    fix 15_3sum

--- 15_3sum/main.c
+++ 15_3sum/main.c
@@ -1,86 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int comp(const void *a, const void *b)
-{
-    return *(int *)a - *(int *)b;
+void AddRes(int** res, int* returnSize, int a, int b, int c){
+    int* resLine = (int*)malloc(3*sizeof(int));
+    resLine[0]  = a;
+    resLine[1]  = b;
+    resLine[2]  = c;
+    res[*returnSize] = resLine;
+    *returnSize = *returnSize + 1;
 }
-void addRes(int **res, int *returnSize, int a, int b, int c)
-{
-    int *resLine = (int *)malloc(sizeof(int) * 3);
-    resLine[0] = a;
-    resLine[1] = b;
-    resLine[2] = c;
-    res[(*returnSize)++] = resLine;
-}
-
-int **threeSumInner(int *nums, int numsSize, int *returnSize, int **returnColumnSizes)
-{
-    if (numsSize < 3)
-    {
-        *returnSize = 0;
-        return NULL;
-    }
-    *returnSize = 0;
-    qsort(nums, numsSize, sizeof(int), comp);
-
-    int **res = (int **)malloc(10000 * sizeof(int *));
-
-    for (int i = 0; i < numsSize; i++)
-    {
-        if (nums[i] > 0)
-        {
-            return res;
-        }
-        if (i > 0 && nums[i] == nums[i - 1])
-            continue;
-        int L = i + 1;
-        int R = numsSize - 1;
-        while (L < R)
-        {
-            int sum = nums[i] + nums[L] + nums[R];
-            if (sum == 0)
-            {
-                addRes(res, returnSize, nums[i], nums[L], nums[R]);
-                while (L < R && nums[L] == nums[L + 1])
-                    L++;
-                while (L < R && nums[R] == nums[R - 1])
-                    R--;
-                L++;
-                R--;
-            }
-            else if (sum > 0)
-            {
-                R--;
-            }
-            else
-            {
-                L++;
-            }
-        }
-    }
-    return res;
-}
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
-int **threeSum(int *nums, int numsSize, int *returnSize, int **returnColumnSizes)
-{
-    int **res = threeSumInner(nums, numsSize, returnSize, returnColumnSizes);
-    if (*returnSize == 0)
-    {
+int** MakeRes(int** res, int *returnSize, int **returnColumnSizes){
+    if(*returnSize == 0){
         free(res);
         return NULL;
     }
-
-    *returnColumnSizes = (int *)malloc((*returnSize) * sizeof(int));
-    for (int i = 0; i < (*returnSize); i++)
-        (*returnColumnSizes)[i] = 3;
+    *returnColumnSizes = (int*)malloc(*returnSize * sizeof(int));
+    for(int i=0;i<*returnSize;i++) (*returnColumnSizes)[i] = 3;
     return res;
 }
-
+int comp(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
+}
+int **threeSum(int *nums, int numsSize, int *returnSize, int **returnColumnSizes){
+    if(nums == NULL || numsSize <3 ){
+        *returnSize = 0;
+        return NULL;
+    }
+    int N = numsSize;
+    qsort(nums, N, sizeof(int), comp);
+    *returnSize = 0;
+    int** res = (int**)malloc(100000*sizeof(int*));
+    for(int i=0;i<N;i++){
+        if(nums[i]>0){
+            return MakeRes(res, returnSize, returnColumnSizes);
+        }
+        if(i>0 && nums[i]== nums[i-1]){
+            continue;
+        } 
+        int L= i+1;
+        int R = N -1;
+        while(L<R){
+            int sum = nums[i] + nums[L] + nums[R];
+            if(sum == 0){
+                AddRes(res, returnSize, nums[i], nums[L], nums[R]);
+                while(L<R && nums[L]==nums[L+1] ) L++;
+                while(L<R && nums[R]==nums[R-1]) R--;
+                L++;
+                R--;
+            }else if(sum < 0){
+                L++;
+            }else{
+                R--;
+            }
+        }
+    }
+    return MakeRes(res, returnSize, returnColumnSizes);
+}
 int main()
 {
 
@@ -106,3 +81,9 @@ int main()
 
     return 0;
 }
+
+/*
+1. 有一个超大输入，有8K多个数字，结果数组需要申请到10000；
+2. 返回为空时，要把*returnSize置为0，free(res)，returnColumnSizes也是NULL，否则会出现：
+AddressSanitizer:DEADLYSIGNAL
+*/
